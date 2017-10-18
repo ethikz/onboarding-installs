3b04e4b0528abec43f80577529e254c75a91c2ec
Josh Christensen <ethikz@users.noreply.github.com>
2017-10-18 12:54:07 -0400

    Update createKey.sh

--- createKey.sh
+++ createKey.sh
@@ -4,23 +4,24 @@
 
 if [ ! -d "$HOME/.ssh" ]; then
   mkdir ~/.ssh
-  cd ~/.ssh || exit
-
-  # shellcheck disable=SC2039
-  read -r -p "What is your work email?" email
-
-  if [ ! -f "$HOME/.ssh/$email" ]; then
-    ssh-keygen -t rsa -C "$email" -P "" -f "$email"
-
-    pbcopy < "$HOME/.ssh/$email"
-    pubkey=$(echo "$HOME/.ssh/$email" | pbcopy)
-    export pubkey
-  fi
-
-  if [ ! -f "$HOME/Downloads/config" ]; then
-    content="$USER\\n$IP\\nIdentityFile ~/.ssh/$email.pub"
-    echo -e "$content" >> ~/Downloads/config
-  fi
-
-  cd .. || exit
 fi
+
+cd ~/.ssh || exit
+
+# shellcheck disable=SC2039
+read -r -p "What is your work email?" email
+
+if [ ! -f "$HOME/.ssh/$email" ]; then
+  ssh-keygen -t rsa -C "$email" -P "" -f "$email"
+
+  pbcopy < "$HOME/.ssh/$email"
+  pubkey=$(echo "$HOME/.ssh/$email" | pbcopy)
+  export pubkey
+fi
+
+if [ ! -f "$HOME/Downloads/config" ]; then
+  content="$USER\\n$IP\\nIdentityFile ~/.ssh/$email.pub"
+  echo -e "$content" >> ~/Downloads/config
+fi
+
+cd ~/ || exit
